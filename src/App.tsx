17a1b31c6ec7d2c98f5439ea77c623df78474688
Hnng2011 <luphihung111@gmail.com>
2024-11-10 22:29:43 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useEthereum, useConnect, useAuthCore } from '@particle-network/auth-core-modal';
-import { Avalanche } from '@particle-network/chains';
-import { AAWrapProvider, SendTransactionMode, SmartAccount } from '@particle-network/aa';
-import { ethers } from 'ethers';
-import { notification } from 'antd';
+import { useState, useEffect } from "react";
+import {
+  useEthereum,
+  useConnect,
+  useAuthCore,
+} from "@particle-network/auth-core-modal";
+import { EthereumSepolia } from "@particle-network/chains";
+import {
+  AAWrapProvider,
+  SendTransactionMode,
+  SmartAccount,
+} from "@particle-network/aa";
+import { ethers } from "ethers";
+import { notification } from "antd";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  const { provider } = useEthereum();
+  const { provider, chainInfo } = useEthereum();
   const { connect, disconnect } = useConnect();
   const { userInfo } = useAuthCore();
 
   const smartAccount = new SmartAccount(provider, {
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    clientKey: process.env.REACT_APP_CLIENT_KEY,
-    appId: process.env.REACT_APP_APP_ID,
+    projectId: "c1b93722-cdab-4f50-b03e-8a997efb3a2e",
+    clientKey: "crQY6HUKI00FPi4uvsFY25pqxm0c390Huf9h1Vd2",
+    appId: "07b9d9c5-47d1-4455-bfbf-43c621aeaf23",
     aaOptions: {
-      simple: [{ chainId: Avalanche.id, version: '1.0.0' }]
-    }
+      simple: [{ chainId: EthereumSepolia.id, version: "1.0.0" }],
+    },
   });
 
-  const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount, SendTransactionMode.Gasless), "any");
+  const customProvider = new ethers.providers.Web3Provider(
+    new AAWrapProvider(smartAccount, SendTransactionMode.Gasless),
+    "any"
+  );
   const [balance, setBalance] = useState(null);
 
   useEffect(() => {
@@ -40,7 +51,7 @@ const App = () => {
     if (!userInfo) {
       await connect({
         socialType: authType,
-        chain: Avalanche,
+        chain: EthereumSepolia,
       });
     }
   };
@@ -54,63 +65,116 @@ const App = () => {
     const txResponse = await signer.sendTransaction(tx);
     const txReceipt = await txResponse.wait();
     notification.success({
-      message: 'Transaction Successful',
+      message: "Transaction Successful",
       description: (
         <div>
-          Transaction Hash: <a href={`https://snowtrace.io/tx/${txReceipt.transactionHash}`} target="_blank" rel="noopener noreferrer">{txReceipt.transactionHash}</a>
+          Transaction Hash:{" "}
+          <a
+            href={`https://snowtrace.io/tx/${txReceipt.transactionHash}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {txReceipt.transactionHash}
+          </a>
         </div>
-      )
+      ),
     });
   };
 
   const executeBatchUserOp = async () => {
-    const tx = { tx: [{
-      to: "0x000000000000000000000000000000000000dEaD",
-      value: ethers.utils.parseEther("0.0001"),
-    },
-    {
-      to: "0x000000000000000000000000000000000000dEaD",
-      value: ethers.utils.parseEther("0.0001"),
-    }]};
+    const tx = {
+      tx: [
+        {
+          to: "0x000000000000000000000000000000000000dEaD",
+          value: ethers.utils.parseEther("0.0001"),
+        },
+        {
+          to: "0x000000000000000000000000000000000000dEaD",
+          value: ethers.utils.parseEther("0.0001"),
+        },
+      ],
+    };
     const txResponse = await smartAccount.sendTransaction(tx);
     notification.success({
-      message: 'Transaction Successful',
+      message: "Transaction Successful",
       description: (
         <div>
-          Transaction Hash: <a href={`https://snowtrace.io/tx/${txResponse}`} target="_blank" rel="noopener noreferrer">{txResponse}</a>
+          Transaction Hash:{" "}
+          <a
+            href={`https://snowtrace.io/tx/${txResponse}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {txResponse}
+          </a>
         </div>
-      )
+      ),
     });
   };
 
   return (
     <div className="App">
       <div className="logo-section">
-        <img src="https://i.imgur.com/EerK7MS.png" alt="Logo 1" className="logo logo-big" />
-        <img src="https://i.imgur.com/eBJAx0s.png" alt="Logo 2" className="logo logo-big" />
+        <img
+          src="https://i.imgur.com/EerK7MS.png"
+          alt="Logo 1"
+          className="logo logo-big"
+        />
       </div>
       {!userInfo ? (
         <div className="login-section">
-          <button className="sign-button google-button" onClick={() => handleLogin('google')}>
-            <img src="https://i.imgur.com/nIN9P4A.png" alt="Google" className="icon"/>
+          <button
+            className="sign-button google-button"
+            onClick={() => handleLogin("google")}
+          >
+            <img
+              src="https://i.imgur.com/nIN9P4A.png"
+              alt="Google"
+              className="icon"
+            />
             Sign in with Google
           </button>
-          <button className="sign-button twitter-button" onClick={() => handleLogin('twitter')}>
-            <img src="https://i.imgur.com/afIaQJC.png" alt="Twitter" className="icon"/>
+          <button
+            className="sign-button twitter-button"
+            onClick={() => handleLogin("twitter")}
+          >
+            <img
+              src="https://i.imgur.com/afIaQJC.png"
+              alt="Twitter"
+              className="icon"
+            />
             Sign in with X
           </button>
-          <button className="sign-button other-button" onClick={() => handleLogin('')}>
-            <img src="https://i.imgur.com/VRftF1b.png" alt="Twitter" className="icon"/>
+          <button
+            className="sign-button other-button"
+            onClick={() => handleLogin("")}
+          >
+            <img
+              src="https://i.imgur.com/VRftF1b.png"
+              alt="Twitter"
+              className="icon"
+            />
           </button>
         </div>
       ) : (
         <div className="profile-card">
           <h2>{userInfo.name}</h2>
           <div className="balance-section">
-            <small>{balance} AVAX</small>
-            <button className="sign-message-button" onClick={executeUserOp}>Execute User Operation</button>
-            <button className="sign-message-button" onClick={executeBatchUserOp}>Execute Batch User Operation</button>
-            <button className="disconnect-button" onClick={disconnect}>Logout</button>
+            <small>
+              {balance} {chainInfo.nativeCurrency.name}
+            </small>
+            <button className="sign-message-button" onClick={executeUserOp}>
+              Execute User Operation
+            </button>
+            <button
+              className="sign-message-button"
+              onClick={executeBatchUserOp}
+            >
+              Execute Batch User Operation
+            </button>
+            <button className="disconnect-button" onClick={disconnect}>
+              Logout
+            </button>
           </div>
         </div>
       )}
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Avalanche } from '@particle-network/chains';
-import { AuthCoreContextProvider } from '@particle-network/auth-core-modal';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EthereumSepolia } from "@particle-network/chains";
+import { AuthCoreContextProvider } from "@particle-network/auth-core-modal";
+import App from "./App";
 
-import('buffer').then(({ Buffer }) => {
+import("buffer").then(({ Buffer }) => {
   window.Buffer = Buffer;
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const options = {
+  projectId: "c1b93722-cdab-4f50-b03e-8a997efb3a2e",
+  clientKey: "crQY6HUKI00FPi4uvsFY25pqxm0c390Huf9h1Vd2",
+  appId: "07b9d9c5-47d1-4455-bfbf-43c621aeaf23",
+  erc4337: {
+    name: "SIMPLE",
+    version: "1.0.0",
+  },
+  wallet: {
+    visible: true,
+    customStyle: {
+      supportChains: [EthereumSepolia],
+    },
+  },
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthCoreContextProvider
-      options={{
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        clientKey: process.env.REACT_APP_CLIENT_KEY,
-        appId: process.env.REACT_APP_APP_ID,
-        erc4337: {
-          name: 'SIMPLE',
-          version: '1.0.0',
-        },
-        wallet: {
-          visible: true,
-          customStyle: {
-              supportChains: [Avalanche],
-          }
-        }
-      }}
-    >
-    <App />
-      </AuthCoreContextProvider>
+    <AuthCoreContextProvider options={options}>
+      <App />
+    </AuthCoreContextProvider>
   </React.StrictMode>
-)
+);
